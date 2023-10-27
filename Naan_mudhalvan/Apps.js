f9bb20af86ea24fde1a3275c877b6b15647e46d9
S.Mohammed Sameer <104424052+ADKsam48@users.noreply.github.com>
2023-10-27 20:53:25 +0530

    Apps.js

--- Naan_mudhalvan/Apps.js
+++ Naan_mudhalvan/Apps.js
@@ -1,12 +1,72 @@
-Function getRandomColor() {
-  Const letters = ‘0123456789ABCDEF’;
-  Let color = ‘#’;
-  For (let I = 0; I < 6; i++) {
-    Color += letters[Math.floor(Math.random() * 16)];
-  }
-  Return color;
+function submitClick() {
+  let name = document.getElementById("name").value;
+  alert("Hey " + name + "\nYour form has been submitted");
 }
-Function changeBackgroundColor() {
-  Document.body.style.backgroundColor = getRandomColor();
+
+"use strict";
+
+let changeBtn = document.querySelector('#change-btn'),
+    hideBtn = document.querySelector('#hide-btn'),
+    title = document.querySelector('.title'),
+    cover = document.createElement('div');
+
+
+changeBtn.addEventListener('click', () => {
+    const randomColor = getRandomColor();
+
+    document.body.style.background = randomColor;
+    changeBtn.style.backgroundColor = randomColor;
+    hideBtn.style.backgroundColor = randomColor;
+    title.textContent = randomColor;
+});
+
+
+changeBtn.addEventListener('click', () => {
+    changeBtn.style.borderColor = 'white';
+    changeBtn.style.color = 'white';
+    hideBtn.style.borderColor = 'white';
+    hideBtn.style.color = 'white';
+},  { once: true });
+
+
+hideBtn.addEventListener('click', function() {
+    hideDisplay();
+    coverBody();
+}); 
+
+ 
+cover.addEventListener('click', showDisplay); 
+
+
+function coverBody() {
+    document.body.appendChild(cover);
+    cover.setAttribute('class', 'cover');
 }
-setInterval(changeBackgroundColor, 3000)
+
+
+function showDisplay() {
+    title.classList.remove('opacity');
+    changeBtn.classList.remove('opacity');
+    hideBtn.classList.remove('opacity');
+
+    cover.classList.remove('cover');
+}
+
+
+function hideDisplay() {
+    title.classList.add('opacity');
+    changeBtn.classList.add('opacity');
+    hideBtn.classList.add('opacity');
+}
+
+
+function getRandomColor() {
+    const num = [];
+
+    for (let i = 0; i < 3; i++) {
+        let randInt = Math.floor(Math.random() * 255);
+        num.push(randInt);
+    }
+
+    return `rgb(${num[0]}, ${num[1]}, ${num[2]})`;
+}
